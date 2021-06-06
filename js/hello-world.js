2c01a4e2e3fa55c9bd1bf3ab82e95b5a90f8ed21
Chatouani <fatee2009@gmail.com>
2021-06-06 17:22:56 +0200

    fix html css index produit

---
+++ js/hello-world.js
@@ -0,0 +1,3 @@
+console.log("Hello World !"); // Va écrire dans la console "Hello World !".
+global.console.log("Hello World !"); // Va écrire dans la console "Hello World !".
+window.console.log("Hello World !"); // Va faire planter le programme.
