309d81d52322d1fd4ea5daceea978d57665db5c6
Chatouani <fatee2009@gmail.com>
2021-06-15 01:22:00 +0200

    fix  get fetch page index et produits

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// lancement de la fonction main 
+main()
+
+// Fonction main qui recupere et affiche les differents produits
+async function main(){
+	const articles = await getArticles()
+	
+	// Boucle pour afficher les differents produits
+	for (article of articles) {
+		displayArticle(article)
+	}
+}
+
+
+// Fonction d'interrogation de l'API
+function getArticles() {
+	return fetch("http://localhost:3000/api/teddies")
+	.then(function(httpBodyResponse) {
+		return httpBodyResponse.json()
+	})
+	.then(function(articles) {
+		return articles
+		// console.log(articles)
+	})
+	.catch(function(error) {
+		alert(error)
+	})
+}
+
+
+// Fonction de templatisation d'un produit
+function displayArticle(article) {
+	document.getElementById("js_ul").innerHTML += `<li class="item">
+		<a href="produit.html?id=${article._id}">
+			<div>
+				<div><img class="nounours_img" src="${article.imageUrl}" alt="visuel ours peluche | Orinoco"></div>
+				<div class="bloc-description">  
+						<div class="description-produit">
+							<h2>${article.name}</h2>
+							<span class="prix">${article.price} €</span>
+						</div>
+				</div>
+			</div>
+		</a>
+	</li>`
+}
--- js/produits.js
+++ js/produits.js
@@ -1,81 +1,73 @@
-const teddies = [
-    {
-      "colors": ["Tan", "Chocolate", "Black", "White"],
-      "_id": "5be9c8541c9d440000665243",
-      "name": "Norbert",
-      "price": 2900,
-      "imageUrl": "teddy_1.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      "colors": [
-        "Pale brown",
-        "Dark brown",
-        "White"
-      ],
-      "_id": "5beaa8bf1c9d440000a57d94",
-      "name": "Arnold",
-      "price": 3900,
-      "imageUrl": "teddy_2.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      "colors": [
-        "Brown"
-      ],
-      "_id": "5beaaa8f1c9d440000a57d95",
-      "name": "Lenny and Carl",
-      "price": 5900,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "imageUrl": "teddy_3.jpg"
-    },
-    {
-      "colors": [
-        "Brown",
-        "Blue",
-        "Pink"
-      ],
-      "_id": "5beaabe91c9d440000a57d96",
-      "name": "Gustav",
-      "price": 4500,
-      "imageUrl": "teddy_4.jpg",
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-    },
-    {
-      "colors": [
-        "Beige",
-        "Tan",
-        "Chocolate"
-      ],
-      "_id": "5beaacd41c9d440000a57d97",
-      "name": "Garfunkel",
-      "price": 5500,
-      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      "imageUrl": "teddy_5.jpg"
-    }
-  ];
+// lancement de la fonction main 
+main()
 
-  
+// Fonction main qui recupere et affiche les differents produits
+async function main(){
 
-for ( var i = 0; i < teddies.length; i++){
+    // On recupere le contenu de la fonction getArticle et on le stock dans la variable articleId
+    const articleId = getArticleId()
+    // On recupere les donnees de l'API ayant comme identifiant l'id placer dans l'URL et on les stocks dans la varaible articleContent
+	const articleContent = await getArticleContent(articleId)
 
-  let nounours = ` <li class="item">
-                        <a href="produit.html?id=` + teddies[i]._id + `">
-                            <div>
-                                <div><img class="nounours_img" src="/JWDP5/images/` + teddies[i].imageUrl + `" alt="visuel ours peluche ` + teddies[i].name + ` | Orinoco"></div>
-                                <div class="bloc-description">
-                                    <div class="description-produit">
-                                        <h2>` + teddies[i].name + `</h2>
-                                        <span>` + teddies[i].colors + `</span>
-                                        <span class="prix">` + teddies[i].price + ` €</span>
-                                    </div>
-                                </div>
-                            </div>
-                          </a>
-                    </li>`;
-  
-  console.log(nounours)
-  
-  document.getElementById("js_ul").innerHTML += nounours;
-
+    displayArticle(articleContent)
 }
+
+// Fonction qui retourne la valeur de ID contenu dans l'url
+function getArticleId() {
+    return new URL(location.href).searchParams.get("id") 
+}
+
+
+// Fonction d'interrogation de l'API
+function getArticleContent(articleId) {
+	return fetch(`http://localhost:3000/api/teddies/${articleId}`)
+	.then(function(httpBodyResponse) {
+		return httpBodyResponse.json()
+	})
+	.then(function(article) {
+		return article
+		//console.log(article)
+	})
+	.catch(function(error) {
+		alert(error)
+	})
+}
+
+
+// Fonction de templatisation d'un produit
+function displayArticle(articleContent) {
+	document.getElementById("js_produit").innerHTML += `<div><img class="nounours_img doudou_img" src="${articleContent.imageUrl}" alt="visuel ours peluche | Orinoco"></div>
+    <div class="bloc-description bloc-descriptions">  
+        <div class="description-produit description-produits">
+            <h2>${articleContent.name}</h2>
+            <span class="prix">${articleContent.price} €</span>
+        </div>
+        <div class="descr">
+            <p>${articleContent.description}</p>
+        </div>
+
+        <div class="choix">
+            <select class="color">
+                <option value="0">Couleur</option>
+                <option value="1">Tan</option>
+                <option value="2">Chocolate</option>
+                <option value="3">Black</option>
+                <option value="4">White</option>
+            </select>
+
+            <select class="color">
+                <option value="0">Quantité</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+            </select>
+        </div>
+    
+        <div class="panierp">
+          <a href="panier.html"> <button class="color colore">Ajouter au panier</button></a>
+        </div>
+        
+    </div>`
+}
+                
